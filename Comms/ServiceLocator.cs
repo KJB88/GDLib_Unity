4af479aa822cc34bac85dce2b72fa683452521be
Kevin <KJB88@users.noreply.github.com>
2024-03-14 17:35:41 +0000

    Refactored ServiceLocator
    + Now uses generics
    + Registers/Unregisters using Type
    + Resolves Type for caller prior to output

--- Comms/ServiceLocator.cs
+++ Comms/ServiceLocator.cs
@@ -1,56 +1,73 @@
+using System;
 using System.Collections.Generic;
 
 namespace GDLib.Comms
 {
     /// <summary>
-    /// Decouple systems that have a large scope from individual sources, minimising use of the Singleton pattern but maintaining large-scope access by low-scope systems
+    /// Allows scoped systems to request access to a scoped services without coupling services directly to systems.
     /// </summary>
     public static class ServiceLocator
-    { 
-        static readonly Dictionary<string, IService> services = new Dictionary<string, IService>();
+    {
+        /// <summary>
+        /// The service registry that holds all services that have registered with the <see cref="ServiceLocator"/>.<para/>
+        /// <typeparamref name="TKey"/> is specified by the <see cref="Type"/> of the service. This is attained via <see langword="typeof"/>(<typeparamref name="T"/>) in relevant methods.<br/>
+        /// <typeparamref name="TValue"/> is the service itself, upcast to <see langword="object"/>.
+        /// </summary>
+        static readonly Dictionary<Type, object> serviceRegistry = new Dictionary<Type, object>();
 
         /// <summary>
-        /// Add a Service to the Service Locator's collection.
+        /// Register the <paramref name="service"/> with the <see cref="ServiceLocator"/>'s service registry, using its <see cref="Type"/> as the key.<para/>
+        /// For replacing an existing service, use <see cref="ReplaceService{T}(T)"/>.<br/>
         /// </summary>
-        /// <param name="serviceKey">string: The ID used by requests to find this service.</param>
-        /// <param name="globalService">GlobalService: The Service itself.</param>
-        public static void AddService(string serviceKey, IService globalService)
+        /// <param name="service">The service to register with the <see cref="ServiceLocator"/>.</param>
+        /// <returns><see langword="true"/> if the <paramref name="service"/> was successfully registered; otherwise, <see langword="false"/>.</returns>
+        public static bool RegisterService<T>(T service)
         {
-            serviceKey = serviceKey.ToLower();
-            if (services.ContainsKey(serviceKey))
-                return;
-            else
-                services.Add(serviceKey, globalService);
+            if (serviceRegistry.ContainsKey(typeof(T)))    
+                return false;                                   
+
+            serviceRegistry.Add(typeof(T), service);       
+            return true;
         }
 
         /// <summary>
-        /// Remove a service from the ServiceLocator.
+        /// Unregister a service from the <see cref="ServiceLocator"/>'s service registry, specified by its <see cref="Type"/>.<para/>
+        /// /// For replacing an existing service, use <see cref="ReplaceService{T}(T)"/>.
         /// </summary>
-        /// <param name="serviceKey">string: The ID of the Service to remove.</param>
-        public static void RemoveService(string serviceKey)
-        {
-            serviceKey = serviceKey.ToLower();
-            if (!services.ContainsKey(serviceKey))
-                return;
-
-            services.Remove(serviceKey);
-        }
+        /// <returns><see langword="true"/> if the service was successfully removed; otherwise, <see langword="false"/>.</returns>
+        public static bool UnregisterService<T>()
+            => serviceRegistry.Remove(typeof(T));
 
         /// <summary>
-        /// Request access to a specific service via its service key.
+        /// Replace a service already registered with the <see cref="ServiceLocator"/> with the given <paramref name="service"/>.<br/>
+        /// An explicit way to reassign a service.<para/> 
+        /// For registering a new service, use <see cref="RegisterService{T}(T)"/>.<br/>
+        /// For unregistering an existing service, use <see cref="UnregisterService{T}"/>.
         /// </summary>
-        /// <param name="serviceKey">string: The ID of the Service to be accessed.</param>
-        /// <param name="outService">GlobalService: The service to out.</param>
-        /// <returns>Bool: Return true if the Service exists with the ServiceLocator. Returns false if the Service does not exist within the ServiceLocator.</returns>
-        public static bool RequestService(string serviceKey, out IService? outService)
+        /// <param name="service">The service that will replace the value within the registry, located at <see langword="typeof"/>.</param>
+        /// <returns><see langname="true"/> if the service was successfully replaced. Else, returns <see langword="false"/>.</returns>
+        public static bool ReplaceService<T>(T service)
         {
-            outService = null;
-            serviceKey = serviceKey.ToLower();
-
-            if (!services.ContainsKey(serviceKey))
+            if (!serviceRegistry.ContainsKey(typeof(T)))
                 return false;
 
-            outService = services[serviceKey];
+            serviceRegistry[typeof(T)] = service;
+            return true;
+        }
+
+        /// <summary> 
+        /// Attempts to locate the <paramref name="service"/> within the <see cref="ServiceLocator"/>'s registry and, if found, assigns it to the <see langword="out"/> parameter. 
+        /// </summary>
+        /// <param name="service">The service to return to caller, located by its <see cref="Type"/>.</param>
+        /// <returns> <see langword="true"/> if the <see cref="ServiceLocator"/>'s registry holds a service of the given <see cref="Type"/>. 
+        /// Else, returns <see langword="false"/>.</returns>
+        public static bool GetService<T>(out T service)
+        {
+            service = default;
+            if (!serviceRegistry.ContainsKey(typeof(T)))
+                return false;
+
+            service = (T)serviceRegistry[typeof(T)];
             return true;
         }
     }
